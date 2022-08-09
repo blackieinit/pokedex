f377894c81ebdf5a8b06ad8220998ce12535edbd
Daniel Parra <danielparra@encodingtech.com>
2022-08-08 23:38:30 -0400

    [ADD] Validations create pokemon

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsNumber()
+    @IsPositive({message: "El número del pokemon debe ser un número positivo"})
+    @Min(1)
+    no: number;
+
+    @IsString({message: "El nombre del pokemon debe ser un string"})
+    @MinLength(1, {message: "El nombre del pokemon debe ser mayor a 1 caracter"})
+    name: string;
+}
