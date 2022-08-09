e9d8785d5dde5db964a1600842681791cbde7a12
Daniel Parra <danielparra@encodingtech.com>
2022-08-08 23:54:48 -0400

    [ADD] Validations error database

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+  
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase()
-    return createPokemonDto;
+
+    try {
+      const pokemon = await this.pokemonModel.create( createPokemonDto );
+      return pokemon;
+
+    } catch (error) {
+      if ( error.code === 11000 ) {
+        throw new BadRequestException(`El dato: ${JSON.stringify( error.keyValue )} se encuentra registrado en la base de datos`);
+      } else {
+        console.log( error );
+        throw new InternalServerErrorException("No se pudo crear el pokemon: error interno en el servidor");
+      }
+    }
   }
 
   findAll() {
